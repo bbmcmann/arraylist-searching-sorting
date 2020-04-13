de68c9fb2f1c669ac198cfa5fb13659e300f6428
Ben McMann <bbmcmann@volosys.com>
2020-04-12 20:36:08 -0600

    Set up functions

--- Sort.java
+++ Sort.java
@@ -1,7 +1,41 @@
+import java.util.*;
 public class Sort{
 
     public static void main(String[] args) {
+        ArrayList<String> artists = new ArrayList<String>();
+        artists.add("Bombay Bicycle Club"); //index 0
+        artists.add("courtship.");
+        artists.add("Future Generations");  //index 2
+        artists.add("Glass House Point");
+        artists.add("Harbour");
+        artists.add("Hippo Campus");        //index 5
+        artists.add("HUNNY");
+        artists.add("Last Dinosaurs");
+        artists.add("Lunar Vacation");
+        artists.add("No Vacation");
+        artists.add("Oh My!");              //index 10
+        artists.add("Peach Pit");
+        artists.add("Remo Drive");
+        artists.add("Rome Hero Foxes");
+        artists.add("Shoobies");
+        artists.add("Strawberry Milk Cult");
+        artists.add("The Greeting Committee");
+        artists.add("The Million");
+        artists.add("The Wrecks");
+        artists.add("Uppermost");
+        artists.add("Wallows");
         
     }
 
+    public static void selectionSort(ArrayList<String> list){
+
+    }
+
+    public static void insertionsSort(ArrayList<String> list){
+
+    }
+
+    public static void bubbleSort(ArrayList<String> list){
+
+    }
 }
